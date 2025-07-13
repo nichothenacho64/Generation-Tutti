9a4ffdf5d4a0a040fada64a3c74d462f9d1eb4c9
Nicclassy <lucanapoli122@gmail.com>
2025-07-13 23:06:49 +0200

    Improve versatility of parsing

--- eda/parsing.py
+++ eda/parsing.py
@@ -1,20 +1,31 @@
 from pathlib import Path
-from typing import ClassVar, Optional, cast
+import re
+from typing import ClassVar, cast
 
 import pandas as pd
 
-from eda.models import AgeRange, ConversationLine, Participant
-from eda.utils import KIPARLA_DATA_PATH, KIPASTI_DATA_PATH
+from eda.models import AgeRange, Conversation, ConversationLine, MacroRegion, Participant, ParticipantLines
+from eda.utils import KIPASTI_DATA_PATH, METADATA_PATH
 
-def _kpc_vert_path(number: int) -> Path:
-    padded_number = str(number).zfill(3)
-    path = KIPASTI_DATA_PATH / "KPC{}.vert.tsv".format(padded_number)
+_DEFAULT_KP_REGION = MacroRegion.CENTRE
+_PARTICIPANT_UNKNOWN = "???"
+
+def _kp_code(number: int, region: MacroRegion) -> str:
+    return "KP{}{}".format(region.short_name, str(number).zfill(3))
+
+def _kp_vert_path(number_or_code: str | int, region: MacroRegion = _DEFAULT_KP_REGION) -> Path:
+    if isinstance(number_or_code, str):
+        return KIPASTI_DATA_PATH / f"{number_or_code}.vert.tsv"
+    padded_number = str(number_or_code).zfill(3)
+    path = KIPASTI_DATA_PATH / "KP{}{}.vert.tsv".format(region.short_name, padded_number)
     assert path.exists(), f"Path {path} does not exist"
     return path
 
-def _kpc_path(number: int) -> Path:
-    padded_number = str(number).zfill(3)
-    path = KIPASTI_DATA_PATH / "KPC{}.csv".format(padded_number)
+def _kp_path(number_or_code: str | int, region: MacroRegion = _DEFAULT_KP_REGION) -> Path:
+    if isinstance(number_or_code, str):
+        return KIPASTI_DATA_PATH / f"{number_or_code}.csv"
+    padded_number = str(number_or_code).zfill(3)
+    path = KIPASTI_DATA_PATH / "KP{}{}.csv".format(region.short_name, padded_number)
     assert path.exists(), f"Path {path} does not exist"
     return path
 
@@ -23,10 +34,11 @@ class Participants:
 
     def __init__(self):
         self._df = self._parse_dataframe()
-        self._participants_by_code = self._parse_participants()
+        self._participants_by_code: dict[str, Participant] = self._parse_participants()
 
-    def __getitem__(self, participant_code: str) -> Participant:
-        return self._participants_by_code[participant_code]
+    def __getitem__(self, code_or_number: str | int) -> Participant:
+        index = f"PKP{str(code_or_number).zfill(3)}" if isinstance(code_or_number, int) else code_or_number
+        return self._participants_by_code[index]
 
     def _parse_participants(self) -> dict[str, Participant]:
         result = {}
@@ -35,13 +47,14 @@ class Participants:
                 cast(str, row.code),
                 cast(str, row.geographic_origin),
                 cast(AgeRange, row.age_range),
-                cast(str, row.mother_tonuge)
+                cast(str, row.mother_tonuge),
+                cast(str, row.in_files)
             )
         return result
 
     @classmethod
     def _parse_dataframe(cls) -> pd.DataFrame:
-        participants_file_path = KIPARLA_DATA_PATH / "metadata" / cls.PARTICIPANTS_FILENAME
+        participants_file_path = METADATA_PATH / cls.PARTICIPANTS_FILENAME
         assert participants_file_path.exists(), f"Path {participants_file_path} does not exist"
         participants_df = pd.read_excel(
             participants_file_path,
@@ -75,48 +88,56 @@ class Participants:
         return participants_df
 
 class ConversationParser:
-    def __init__(
-        self,
-        participants: Participants,
-        *,
-        conversation_number: Optional[int] = None,
-        kpc_df: Optional[pd.DataFrame] = None,
-        kpc_vert_df: Optional[pd.DataFrame] = None
-    ):
-        if kpc_df is None and kpc_vert_df is None:
-            assert conversation_number is not None
-            kpc_df, kpc_vert_df = self.parse_dataframes(conversation_number)
-        assert kpc_df is not None and kpc_vert_df is not None
+    def __init__(self, participants: Participants):
         self._participants = participants
-        self._kpc_df = kpc_df
-        self._kpc_vert_df = kpc_vert_df
 
-    @classmethod
-    def parse_dataframes(cls, conversation_number: int) -> tuple[pd.DataFrame, pd.DataFrame]:
-        kpc_path = _kpc_path(conversation_number)
-        kpc_vert_path = _kpc_vert_path(conversation_number)
+    def parse_conversation(self, number_or_code: str | int, region: MacroRegion = _DEFAULT_KP_REGION) -> Conversation:
+        kpc_path = _kp_path(number_or_code, region)
+        kpc_vert_path = _kp_vert_path(number_or_code, region)
         kpc_df = pd.read_csv(kpc_path, sep='\t')
         kpc_vert_df = pd.read_csv(kpc_vert_path, sep='\t')
-        return (kpc_df, kpc_vert_df)
 
-    def parse_conversation(self) -> list[ConversationLine]:
         result = []
         normalised_words = []
-        kpc_rows = self._kpc_df.itertuples()
+        kpc_rows = kpc_df.itertuples()
         current_tu_id = 0
 
-        for row in self._kpc_vert_df.itertuples():
+        for row in kpc_vert_df.itertuples():
             tu_id = row.tu_id
             word = row.form
             if current_tu_id != tu_id:
                 kpc_row = next(kpc_rows)
-                conversation_line = ConversationLine(
-                    self._participants[cast(str, row.speaker)],
-                    cast(str, kpc_row.text),
-                    normalised_words[:]
-                )
-                result.append(conversation_line)
+                if row.speaker != _PARTICIPANT_UNKNOWN:
+                    conversation_line = ConversationLine(
+                        self._participants[cast(str, row.speaker)],
+                        cast(str, kpc_row.text),
+                        normalised_words[:]
+                    )
+                    result.append(conversation_line)
                 normalised_words.clear()
                 current_tu_id = tu_id
             normalised_words.append(word)
-        return result
+        return Conversation(pd.Series(result))
+
+class Conversations:
+    def __init__(self, participants: Participants):
+        self._parser = ConversationParser(participants)
+        self._conversations: dict[str, Conversation] = {}
+
+    def conversation(self, number_or_code: str | int, region: MacroRegion = _DEFAULT_KP_REGION) -> Conversation:
+        if isinstance(number_or_code, str):
+            code = number_or_code
+        else:
+            code = _kp_code(number_or_code, region)
+
+        if (conversation := self._conversations.get(code)) is not None:
+            return conversation
+
+        self._conversations[code] = conversation = self._parser.parse_conversation(code)
+        return conversation
+
+    def participant_lines(self, participant: Participant) -> ParticipantLines:
+        conversation = self.conversation(participant.conversation_code)
+        lines_filter = conversation.lines.map(lambda line: line.participant == participant)
+        return ParticipantLines(participant, conversation.lines[lines_filter])
+
--- eda/utils.py
+++ eda/utils.py
@@ -1,10 +1,16 @@
 from pathlib import Path
-from typing import Iterator
+from typing import Any, Callable, Iterator
+
+import pandas as pd
 
 FOLDER_DIR = Path(__file__).resolve().parent.parent
 
 KIPARLA_DATA_PATH = FOLDER_DIR / "kiparla-data"
 KIPASTI_DATA_PATH = KIPARLA_DATA_PATH / "kipasti-data"
+METADATA_PATH = KIPARLA_DATA_PATH / "metadata"
 
-def truthy_list[T](value: Iterator[T]) -> list[T]:
-    return list(filter(None, value))
+def truthy_tuple[T](value: Iterator[T]) -> tuple[T, ...]:
+    return tuple(filter(None, value))
+
+def filter_map_truthy(series: pd.Series, func: Callable[[pd.Series], Any]) -> pd.Series:
+    return series[series.apply(lambda row: bool(func(row)))].map(func)
